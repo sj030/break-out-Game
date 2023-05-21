80b89f69569e8fbebfcfdf86fbe138636c201927
changsam <edwarp7@naver.com>
2023-05-21 16:02:58 +0900

    add refernce

--- game/ball.js
+++ game/ball.js
@@ -2,12 +2,16 @@
 
 class Ball {
     constructor(imagesrc) {
+        //x,y는 캔버스의 좌표
         this.x = 400;
         this.y = 500;
+        //width랑 height는 공의 크기
         this.width = 40;
         this.height = 40;
+        //dir은 공의 속도
         this.xdir = 5;
         this.ydir = 5;
+        //공 이미
         this.ballImage = new Image();
         this.ballImage.src = imagesrc;
     }
