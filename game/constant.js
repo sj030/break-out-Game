40731475061a29cde57198a822ccbe364b136a86
sj030 <hsj3151120@konkuk.ac.kr>
2023-05-25 04:52:22 +0900

    add pageChange effect

--- game/constant.js
+++ game/constant.js
@@ -1,6 +1,6 @@
 "use strict";
 // 전역변수 작성 영역
-
+// 황서진]게임 화면 크기는 width: 1280px; height: 920px; 를 기준으로 하겠습니다. 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
